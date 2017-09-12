8b8f4a16b0c4ba8b9aa9b39132e6da75febf8add
Jeroen Wellner <jeroen@wllnr.nl>
2017-09-12 16:56:04 +0200

    update dist to www

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git commit -am "Saved local changes"
 git checkout -B gh-pages
-git add -f dist
+git add -f www
 git commit -am "Deployed website"
-git filter-branch -f --prune-empty --subdirectory-filter dist
+git filter-branch -f --prune-empty --subdirectory-filter www
 git push -f origin gh-pages
 git checkout -
